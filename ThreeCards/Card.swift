78300e16c0334db8c836d7e61d2d1410f33b0e5d
crowcasso <crowcasso@gmail.com>
2017-09-25 08:51:27 -0400

    Card

---
+++ ThreeCards/Card.swift
@@ -0,0 +1,47 @@
+//
+//  Card.swift
+//  ThreeCards
+//
+//  Created by Joel Hollingsworth on 9/25/17.
+//  Copyright © 2017 Joel Hollingsworth. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Card {
+    
+    var value: String
+    var suit: String
+    
+    enum CardState {
+        case up
+        case down
+    }
+    
+    var state = CardState.down
+    
+    var upImage: UIImage
+    var downImage = #imageLiteral(resourceName: "back_of_card")
+    
+    var text: String
+    
+    // initializer for the class
+    init(_ value: String, _ suit: String) {
+        self.value = value
+        self.suit = suit
+        
+        upImage = UIImage(named: "\(value)_of_\(suit)")!
+        
+        text = "\(value) of \(suit)"
+    }
+    
+    // the current image based on the card state
+    func getCurrentImage() -> UIImage {
+        if state == .up {
+            return upImage
+        }
+        
+        return downImage
+    }
+}
